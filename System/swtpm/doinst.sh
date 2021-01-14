5ea8329c94ce3f4971f5a30f3b6b2e27e28f29af
spinl0ck <spinl0ck@protonmail.com>
2021-01-14 22:10:00 +0300

    Add package scripts for libtpms and swtpm

---
+++ System/swtpm/doinst.sh
@@ -0,0 +1,25 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/swtpm-setup.conf.new
+config etc/swtpm-localca.options.new
+config etc/swtpm-localca.conf.new
+
